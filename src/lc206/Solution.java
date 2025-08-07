42915b507fc5576a6bfcc197ef38b029545048b4
DylanGzHp <dorajam0501@gmail.com>
2025-08-07 21:48:44 +0800

    0807

--- src/lc206/Solution.java
+++ src/lc206/Solution.java
@@ -2,6 +2,9 @@ package lc206;
 
 import data_structure.list.ListNode;
 
+import java.sql.Array;
+import java.util.ArrayList;
+
 public class Solution {
 
 
@@ -27,7 +30,47 @@ public class Solution {
         return prev;
     }
 
+    public ListNode reverseList2(ListNode head){
+        ArrayList<ListNode> listNodes = new ArrayList<>(1);
+        recursively(head, listNodes);
+        return listNodes.get(0);
+    }
 
+    public ListNode recursively(ListNode head, ArrayList<ListNode> dummy){
+
+        if (head == null || head.next == null){
+            dummy.add(head);
+            return head;
+        }
+        ListNode listNode = recursively(head.next, dummy);
+
+        listNode.next = head;
+        head.next = null;
+        return head;
+
+    }
+
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        ListNode second = new ListNode(2);
+        ListNode first = new ListNode(1, second);
+
+        ListNode listNode = solution.reverseList2(first);
+        System.out.println(3);
+    }
+
+    // 1 <- 2 -> 3 -> 4
+
+    public ListNode reverseList3(ListNode head){
+        if (head == null || head.next == null){
+            return head;
+        }
+        ListNode listNode = reverseList3(head);
+        head.next.next = head;
+        head.next = null;
+        return listNode;
+    }
 
 
 
